ccd97a0b3906ca7b244536e5837eeb780d497f31
d.ilchenko <d.ilchenko@plarium.com>
2019-11-28 23:43:41 +0200

    FPS counter

---
+++ Assets/Script/FPSDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class FPSDisplay : MonoBehaviour
+{
+  public int avgFrameRate;
+  public Text display_Text;
+ 
+  public void Update ()
+  {
+    var fps = Time.frameCount / Time.time;
+    display_Text.text = (int)fps + " FPS";
+  }
+}
